327213b29ce1c83e6deb2ca7c16b0b5419da91ec
Salvador Lopez Picazo <salvadorlopezpicazo@gmail.com>
2026-03-14 13:56:44 -0600

    prueba de librerias

---
+++ prueba.py
@@ -0,0 +1,9 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+
+print("--- Verificación de Sistema ---")
+print("OpenCV:", cv2.__version__)
+print("MediaPipe:", "Listo")
+print("NumPy:", np.__version__)
+print("-------------------------------")
