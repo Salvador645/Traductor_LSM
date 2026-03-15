c3f37c6ff435c859cb0ec46b035bcef2f3e520c4
Salvador Lopez Picazo <salvadorlopezpicazo@gmail.com>
2026-03-14 20:12:47 -0600

    version 1 con pocas palabras

--- detector_manos.py
+++ detector_manos.py
@@ -146,7 +146,7 @@ def detectar_lsm(hand):
 # -----------------------------
 # CAMARA
 # -----------------------------
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 cap.set(3, 640)
 cap.set(4, 480)
